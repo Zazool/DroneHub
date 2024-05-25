de94fb72df278ae8ec5e285dc53f004a9424401a
Liam.M <liam.mead@live.co.uk>
2024-05-25 19:11:23 +0100

    working

--- src/pages/forms/PhotographyForm.js
+++ src/pages/forms/PhotographyForm.js
@@ -1,5 +1,7 @@
 // src/pages/PhotographyForm.js
 import React from 'react';
+import { Formik } from 'formik';
+import { Form } from 'antd';
 import CustomInput from './components/CustomInput.js';
 import CustomButton from './components/CustomButton';
 import '../../styles/global/GlobalForms.css';
@@ -7,18 +9,24 @@ import '../../styles/global/GlobalForms.css';
 const PhotographyForm = () => {
   return (
     <div className="form-container">
-      <h2>Photography Service Form</h2>
-      <form>
-        <div>
-          <label>Number of Photos:</label>
-          <input type="number" name="numPhotos" />
-        </div>
-        <div>
-          <label>Event Type:</label>
-          <input type="text" name="eventType" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      <h2 className="text-3xl font-bold text-white mb-4">Drone Service Form</h2>
+      <Formik
+        initialValues={{ flightDuration: '', equipmentType: '' }}
+        onSubmit={(values, { setSubmitting }) => {
+          // handle form submission here
+          setSubmitting(false);
+        }}
+      >
+        {({ handleSubmit, isSubmitting }) => (
+          <Form onFinish={handleSubmit} className="custom-form">
+            <CustomInput label="Flight Duration" name="flightDuration" type="number" />
+            <CustomInput label="Equipment Type" name="equipmentType" type="text" />
+            <Form.Item>
+              <CustomButton text="Submit" htmlType="submit" disabled={isSubmitting} />
+            </Form.Item>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
--- src/pages/forms/VideographyForm.js
+++ src/pages/forms/VideographyForm.js
@@ -1,5 +1,7 @@
 // src/pages/VideographyForm.js
 import React from 'react';
+import { Formik } from 'formik';
+import { Form } from 'antd';
 import CustomInput from './components/CustomInput.js';
 import CustomButton from './components/CustomButton';
 import '../../styles/global/GlobalForms.css';
@@ -7,20 +9,26 @@ import '../../styles/global/GlobalForms.css';
 const VideographyForm = () => {
   return (
     <div className="form-container">
-      <h2>Videography Service Form</h2>
-      <form>
-        <div>
-          <label>Video Length:</label>
-          <input type="number" name="videoLength" />
-        </div>
-        <div>
-          <label>Event Type:</label>
-          <input type="text" name="eventType" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      <h2 className="text-3xl font-bold text-white mb-4">Drone Service Form</h2>
+      <Formik
+        initialValues={{ flightDuration: '', equipmentType: '' }}
+        onSubmit={(values, { setSubmitting }) => {
+          // handle form submission here
+          setSubmitting(false);
+        }}
+      >
+        {({ handleSubmit, isSubmitting }) => (
+          <Form onFinish={handleSubmit} className="custom-form">
+            <CustomInput label="Flight Duration" name="flightDuration" type="number" />
+            <CustomInput label="Equipment Type" name="equipmentType" type="text" />
+            <Form.Item>
+              <CustomButton text="Submit" htmlType="submit" disabled={isSubmitting} />
+            </Form.Item>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
-};
+}
 
 export default VideographyForm;
