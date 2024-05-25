bb8f84ae333038b7340ca416706fae145d026436
Liam.M <liam.mead@live.co.uk>
2024-05-25 18:34:10 +0100

    working

--- src/pages/forms/DroneForm.js
+++ src/pages/forms/DroneForm.js
@@ -1,15 +1,15 @@
-// src/pages/Forms/DroneForm.js
+// src/pages/DroneForm.js
 import React from 'react';
-import { Formik, Field } from 'formik';
-import { Form } from 'antd';
+import { Formik } from "formik";
+import { Form } from "antd";
+import '../../styles/global/GlobalForms.css';
 import CustomInput from './components/CustomInput.js';
 import CustomButton from './components/CustomButton';
-import '../../styles/global/GlobalForms.css';
 
 const DroneForm = () => {
   return (
     <div className="form-container">
-      <h2 className="text-3xl font-bold text-white mb-4">Drone Service Form</h2>
+      <h2>Drone Service Form</h2>
       <Formik
         initialValues={{ flightDuration: '', equipmentType: '' }}
         onSubmit={(values, { setSubmitting }) => {
@@ -18,7 +18,7 @@ const DroneForm = () => {
         }}
       >
         {({ handleSubmit, isSubmitting }) => (
-          <Form onFinish={handleSubmit} className="custom-form">
+          <Form onFinish={handleSubmit} className="drone-form">
             <CustomInput label="Flight Duration" name="flightDuration" type="number" />
             <CustomInput label="Equipment Type" name="equipmentType" type="text" />
             <Form.Item>
